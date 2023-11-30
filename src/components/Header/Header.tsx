55b6a654981c6d9a9133d8253c37fbb48cfafbff
RamziNaili <naili.ramzi@outlook.fr>
2023-11-30 13:42:50 +0100

    feat: add fonts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ import {
 export const Header: FC = () => {
   return (
     <div className="flex justify-between items-center p-8">
-      <p className="font-popins font-bold text-[#2d2e32] text-xl">
+      <p className="font-poppins font-bold text-[#2d2e32] text-xl">
         Naili Ramzi
       </p>
       <div className="md:hidden flex items-center">
@@ -21,16 +21,16 @@ export const Header: FC = () => {
           <SheetContent side="left" className="w-screen h-screen">
             <SheetDescription className="flex items-center h-full w-full">
               <div className="flex flex-col w-full items-center gap-10">
-                <p className="font-popins text-2xl font-semibold text-black">
+                <p className="font-poppins text-2xl font-semibold text-black">
                   Home
                 </p>
-                <p className="font-popins text-2xl font-semibold text-black">
+                <p className="font-poppins text-2xl font-semibold text-black">
                   About
                 </p>
-                <p className="font-popins text-2xl font-semibold text-black">
+                <p className="font-poppins text-2xl font-semibold text-black">
                   Projects
                 </p>
-                <p className="font-popins text-2xl font-semibold text-black">
+                <p className="font-poppins text-2xl font-semibold text-black">
                   Contact
                 </p>
               </div>
