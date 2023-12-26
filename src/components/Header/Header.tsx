33892cc31808092c464167019da88d644d23ed99
RamziNaili <naili.ramzi@outlook.fr>
2023-12-26 13:10:20 +0100

    feat: improve responsivness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ import {
 
 export const Header: FC = () => {
   return (
-    <div className="flex justify-between items-center p-8 shadow-[0_0px_11px_rgba(0,0,0,.09)]">
+    <div className="flex justify-between items-center px-[50px] py-[25px] shadow-[0_0px_11px_rgba(0,0,0,.09)]">
       <p className="font-poppins font-bold text-[#2d2e32] text-xl">
         Naili Ramzi
       </p>
@@ -20,7 +20,7 @@ export const Header: FC = () => {
           </SheetTrigger>
           <SheetContent side="left" className="w-screen h-screen">
             <SheetDescription className="flex items-center h-full w-full">
-              <div className="flex flex-col w-full items-center gap-10">
+              <div className="flex flex-col w-full items-center gap-10 hover:cursor-pointer">
                 <p className="font-poppins text-2xl font-semibold text-black">
                   Home
                 </p>
@@ -38,11 +38,19 @@ export const Header: FC = () => {
           </SheetContent>
         </Sheet>
       </div>
-      <div className="flex gap-3 max-md:hidden">
-        <p className="font-popins text-lg font-semibold text-black">Home</p>
-        <p className="font-popins text-lg font-semibold text-black">About</p>
-        <p className="font-popins text-lg font-semibold text-black">Project</p>
-        <p className="font-popins text-lg font-semibold text-black">Contact</p>
+      <div className="flex gap-4 max-md:hidden">
+        <p className="font-popins text-lg font-semibold hover:cursor-pointer hover:text-sky-600">
+          Home
+        </p>
+        <p className="font-popins text-lg font-semibold hover:cursor-pointer hover:text-sky-600">
+          About
+        </p>
+        <p className="font-popins text-lg font-semibold  hover:cursor-pointer hover:text-sky-600">
+          Project
+        </p>
+        <p className="font-popins text-lg font-semibold hover:cursor-pointer hover:text-sky-600">
+          Contact
+        </p>
       </div>
     </div>
   );
--- src/components/Presentation/Presentation.tsx
+++ src/components/Presentation/Presentation.tsx
@@ -4,30 +4,30 @@ import LinkedinSvg from '@/assets/linkedin.svg?react';
 
 export const Presentation: FC = () => {
   return (
-    <div className="flex justify-center gap-20 items-center mt-24 max-md:flex-col md:flex-row-reverse">
+    <div className="flex justify-center gap-20  mt-16 md:flex-row-reverse">
       <img
         src="/ranaili.jpg"
         alt="profile"
-        className="border-2 border-[#2d2e32] rounded-full h-64 w-64 max-lg:w-1/4 max-lg:h-1/4 max-md:w-2/4"
+        className="border-2 border-[#2d2e32] rounded-full h-96 w-96 "
       />
-      <div className="flex flex-col gap-10 relative max-sm:items-center">
-        <div className="flex items-center max-sm:justify-center">
-          <h1 className="font-black text-6xl text-[#2d2e32] font-poppins w-[500px] relative max-sm:w-4/5">
+      <div className="flex flex-col gap-6 relative ">
+        <div className="flex items-center ">
+          <h1 className="font-black text-6xl text-[#2d2e32] font-poppins w-[500px] relative mt-16">
             Front-End React Developer
           </h1>
           <img
             src="/waving.png"
             alt="waving_hand"
-            className="h-14 w-14 absolute top-[4rem] left-[20rem]"
+            className="h-14 w-14 absolute top-[8rem]  left-[20rem]"
           />
         </div>
-        <p className="font-sans text-lg text-[#55555] w-[500px] max-sm:w-4/5">
+        <p className="font-sans text-lg text-[#55555] w-[500px]">
           Hi, I'm Ramzi Naili. A passionate Front-end React Developer based in
           Paris, France. 📍
         </p>
         <div className="flex gap-4">
-          <LinkedinSvg className="hover:cursor-pointer hover:text-sky-500" />
-          <GithubSvg className="hover:cursor-pointer hover:text-sky-500" />
+          <LinkedinSvg className="hover:cursor-pointer hover:text-sky-600" />
+          <GithubSvg className="hover:cursor-pointer hover:text-sky-600" />
         </div>
       </div>
     </div>
